d39ba54b9a33c06203842e3252dac0b852708a62
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-09 17:05:03 -0600

    324. How to Import the Image Recognition Model & Enable Camera Functionality

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+	// MARK: - IBOutlets
+	
+	@IBOutlet weak var imageView: UIImageView!
+	
+	// MARK: - Objects/variables
+	
+	let imagePicker = UIImagePickerController()
+	
+	// MARK: - UIViewController
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		imagePicker.delegate = self
+		//imagePicker.sourceType = .camera
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.allowsEditing = false
     }
-
-
+	
+	// MARK: - IBActions
+	
+	@IBAction func cameraTapped(_ sender:  UIBarButtonItem) {
+		present(imagePicker, animated: true, completion: nil )
+	}
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	
+	// MARK: - UIImagePickerControllerDelegate
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+		imageView.image = image
+		imagePicker.dismiss(animated: true, completion: nil)
+	}
+}
